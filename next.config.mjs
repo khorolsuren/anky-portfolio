986744d06b77b30e488210a01cbff8a3a799be6d
Khorolsuren Dashdavaa <khorolsuren1111@gmail.com>
2025-09-01 17:29:04 -0400

    change

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",  // for static export
+    output: "export",
     basePath: "/anky-portfolio",
     assetPrefix: "/anky-portfolio/",
     images: {
